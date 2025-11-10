b1fe13d24a82ea832e15cbf160cbd6a0fc233619
santiagoMD297 <santiago.marindiaz.297@my.aquinas.me>
2025-11-10 13:38:49 +0000

    1

---
+++ BST.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, value):
+        """
+        A node in a binary search tree.
+
+        :param value: The value stored in the node.
+        """
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class Tree:
+    def __init__(self):
+        """
+        A binary search tree.
+        """
+        self.root = None
+
+    def insert(self, value):
+        """
+        Inserts a value into the binary search tree.
+        """
+        if self.root is None:
+            
+        pass
+
+    def search(self, value):
+        """
+        Searches for a value in the binary search tree.
+        Returns True if found, False otherwise.
+        """
+        # Students will implement this
+        pass
+
+    def inorder_traversal(self):
+        """
+        Returns a list of values from an in-order traversal of the tree.
+        """
+        # Students will implement this
+        pass
